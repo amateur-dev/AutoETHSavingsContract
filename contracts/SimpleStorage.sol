f446c3878088d4e8e17829e420792ed2cf9f872b
amateur_dev <49892677+amateur-dev@users.noreply.github.com>
2019-07-20 17:09:39 +0800

    Add files via upload

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract SimpleStorage {
+  string storedData;
+
+  function set(string memory _name) payable public {
+    storedData = _name;
+  }
+
+  function get() public view returns (string memory) {
+    return storedData;
+  }
+}

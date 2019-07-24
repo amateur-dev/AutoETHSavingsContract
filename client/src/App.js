9fe020f46324de25dddcb3fcdfe14ea3e6a3afdb
amateur-dev <dipesh.sukhani@gmail.com>
2019-07-24 13:52:44 +0800

    Update the App.js with a button to deploy the contract

--- client/src/App.js
+++ client/src/App.js
@@ -9,34 +9,49 @@ class App extends Component {
     super(props);
     this.state = { storageValue: "No value has been set", web3: null, accounts: null, contract: null };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    // this.handleChange = this.handleChange.bind(this);
+    this.deployContract = this.deployContract.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
   }
   // state = { storageValue: null, web3: null, accounts: null, contract: null };
 
 
-  handleChange(event) {
+  deployContract = async (event) => {
     event.preventDefault();
-    console.log("initiating the handle change event")
-    const newValue = this.element.value;
-    console.log("newValue is " + newValue);
+    console.log('calling the deploy contract fx');
+    const web3 = this.state.web3
+    console.log("deploying the contract")
     const accounts = this.state.accounts;
-    const contract = this.state.contract;
+    console.log(accounts)
+    await new web3.eth.Contract(SimpleStorageContract.abi).deploy({
+      data: SimpleStorageContract.bytecode,}).send({ from: accounts[0], gas: '1000000' })
+      .on('error', (error) => {console.log(error)})
+      .on('transactionHash', (transactionHash) => {console.log(transactionHash)})
+      .on('receipt', (receipt) => {console.log(receipt.contractAddress)});
+      };
+  
 
-    async function newfuntion() {
-      await contract.methods.set(newValue).send({ from: accounts[0] });
-    };
-    newfuntion();
+  // handleChange(event) {
+  //   event.preventDefault();
+  //   console.log("initiating the handle change event")
+  //   const newValue = this.element.value;
+  //   console.log("newValue is " + newValue);
+  //   const accounts = this.state.accounts;
+  //   const contract = this.state.contract;
 
-    this.setState({ storageValue: newValue });
+  //   async function newfuntion() {
+  //     await contract.methods.set(newValue).send({ from: accounts[0] });
+  //   };
+  //   newfuntion();
+
+  //   this.setState({ storageValue: newValue });
+  // }
     // 
 
-  }
-
-  handleSubmit(event) {
-    // alert('A name was submitted: ' + this.state.storageValue);
-    event.preventDefault();
-  }
+  // handleSubmit(event) {
+  //   // alert('A name was submitted: ' + this.state.storageValue);
+  //   event.preventDefault();
+  // }
 
   componentDidMount = async () => {
     try {
@@ -50,18 +65,18 @@ class App extends Component {
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
       console.log(networkId);
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      console.log(deployedNetwork)  // this pulls out the object of the contract which gives the contract storage address and etc
-      console.log(deployedNetwork.address)
+      // const deployedNetwork = SimpleStorageContract.networks[networkId];
+      // console.log(deployedNetwork)  // this pulls out the object of the contract which gives the contract storage address and etc
+      // console.log(deployedNetwork.address)
 
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
+      // const instance = new web3.eth.Contract(
+      //   SimpleStorageContract.abi,
+      //   deployedNetwork && deployedNetwork.address,
+      // );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({ web3, accounts});
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -71,18 +86,18 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
+  // runExample = async () => {
+  //   const { accounts, contract } = this.state;
 
-    // Stores a given value, 5 by default.
-    await contract.methods.set("new value").send({ from: accounts[0] });
+  //   // Stores a given value, 5 by default.
+  //   await contract.methods.set("new value").send({ from: accounts[0] });
 
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
+  //   // Get the value from the contract to prove it worked.
+  //   const response = await contract.methods.get().call();
 
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
+  //   // Update state with the result.
+  //   this.setState({ storageValue: response });
+  // };
 
 
 
@@ -93,17 +108,22 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
+        <h2>Would you like to depoly your Petty Cash Savings Smart Contract</h2>
+        <button onClick={this.deployContract}>Submit</button>
+        {/* <form onSubmit={this.deployContract}>
+          <input type="submit" value="Submit"/>
+        </form> */}
+        {/* <p>Your Truffle Box is installed and ready.</p>
         <h2>Smart Contract Example</h2>
         <p>
           If your contracts compiled and migrated successfully, below will show
           that "No value has been set" in the Smart Contract.
         </p>
-        <p>
+        <p> */}
           {/* Try changing the value stored on <strong>line 40</strong> of App.js. */}
-          Use the form below to update the value of the string stored in the smart contract:
-        </p>
-        <form onSubmit={this.handleChange}>
+          {/* Use the form below to update the value of the string stored in the smart contract:
+        </p> */}
+        {/* <form onSubmit={this.handleChange}>
           <label>
             Name:
             <input type="text" name="name" ref={el => this.element = el} />
@@ -111,7 +131,7 @@ class App extends Component {
           <input type="submit" value="Submit" />
         </form>
 
-        <div>The stored value is: {this.state.storageValue}</div>
+        <div>The stored value is: {this.state.storageValue}</div> */}
       </div>
     );
   }
